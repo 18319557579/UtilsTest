d5479efd4208edc0f0b21a8e6fc3bfa25cdefd03
ALittleDaisy <1152594956@qq.com>
2023-07-26 17:43:38 +0800

    增加一个带复制按钮的文本分享功能

--- app/src/main/java/com/hsf/utilstest/share/ShareTest.java
+++ app/src/main/java/com/hsf/utilstest/share/ShareTest.java
@@ -11,4 +11,15 @@ public class ShareTest {
         intent.setType("text/plain");
         activity.startActivity(intent);
     }
+
+    /**
+     * 系统分享，纯文本，但是多一个复制按钮
+     */
+    public static void shareText_withCopy(Activity activity, String shareContent) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, shareContent);
+        intent.setType("text/plain");
+        activity.startActivity(Intent.createChooser(intent ,"分享"));
+    }
 }
